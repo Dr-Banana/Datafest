2da95af22f1d136ffb67e4e788b18d87ad49c8ef
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-02 15:24:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,27 @@ st.title("Zhang_10419_lab3_code")
 # Set Up
 data = pd.read_csv("https://raw.githubusercontent.com/Dr-Banana/Datafest/main/S5_scores_cleaned.csv")
 
-df_1 = data.drop(columns=['player_id'])
+chart_data =  data.drop(columns=['player_id'])
 wk = [0,3,6,12,24]
 
-chart_data = pd.DataFrame()
-for n in range(5):
-  count = 0
-  mean_list = []
-  std_list = []
-  df_tmp = df_1[df_1["weeks"]== wk[n]]
-  mn = sum(mean_list)/len(mean_list)
-  chart_data = {wk[n]:mn}
-  chart_data = pd.DataFrame(data=chart_data)
 chart_data
+singleSelect = st.selectbox("week", wk)
+filter_data = chart_data[chart_data['weeks'] == singleSelect]
+tit2 = "Box plot for " + singleSelect 
+upper = alt.Chart(filter_data).mark_rect().encode(
+    y = alt.Y('emission:Q', bin=alt.Bin(maxbins=30)),
+    x = 'year:O',
+    color = alt.Color('emission:Q', scale=alt.Scale(scheme='greenblue')),
+    tooltip=['country', 'year', 'emission']
+).properties(width=550,height=200,
+    title= tit1
+)
+lower = alt.Chart(filter_data).mark_boxplot(size=50,extent=0.5).encode(
+    x='country',
+    y=alt.Y('emission:Q',scale=alt.Scale(zero=False))
+).properties(width=550,
+    title= tit2
+)
+
+obj = alt.vconcat(upper, lower)
+st.altair_chart(lower)
