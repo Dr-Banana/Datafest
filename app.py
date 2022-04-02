e18272c11e57c4e5d80455e0fe469c6dff734069
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-02 15:15:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ st.title("Zhang_10419_lab3_code")
 data = pd.read_csv("https://raw.githubusercontent.com/Dr-Banana/Datafest/main/S5_scores_cleaned.csv")
 
 df_1 = data.drop(columns=['player_id'])
-df_1['weeks']
+wk = [0,3,6,12,24]
+
 chart_data = pd.DataFrame()
+for n in range(5):
+  count = 0
+  mean_list = []
+  std_list = []
+  df_tmp = df_1[df_1["weeks"]== wk[n]]
+  mn = sum(mean_list)/len(mean_list)
+  chart_data = {wk[n]:mn}
+  chart_data = pd.DataFrame(data=chart_data)
+chart_data
